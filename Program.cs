8fe776f14a4209d54a3e30162d8068df12a2b316
Marshall Mabee <marshallmabee@gmail.com>
2016-11-28 11:59:14 -0700

    I cant get a program running :(

--- Program.cs
+++ Program.cs
@@ -4,20 +4,18 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        public static myGame = new Game();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
           
         }
-        public int myInt = 10;
 
         public static void MyFunction () {
-                Console.WriteLine("Run Function");
+               
+                
         }
 
-        public static int IntFunction (int _peram) {
-            return _peram + myInt;
-        }
+       
     }
 
 }
---
+++ script/game.cs
@@ -0,0 +1,9 @@
+using System;
+public class Game {
+    
+ public void StartGame() {
+     Console.WriteLine("Program is Running");
+
+ }
+
+}
