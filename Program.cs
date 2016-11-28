c572231d0df6a12f84fe49cd263cc634f4578f6e
Marshall Mabee <marshallmabee@gmail.com>
2016-11-28 11:02:29 -0700

    Begin

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          
+        }
+        public int myInt = 10;
+
+        public static void MyFunction () {
+                Console.WriteLine("Run Function");
+        }
+
+        public static int IntFunction (int _peram) {
+            return _peram + myInt;
+        }
+    }
+
+}
